07f3c25692123f3615d5251c8acb6efb635fc151
horsicq <horsicq@gmail.com>
2021-10-24 19:02:43 +0200

    Fix: 2021-10-24

---
+++ xdemanglewidget.cmake
@@ -0,0 +1,8 @@
+include_directories(${CMAKE_CURRENT_LIST_DIR})
+
+set(XDEMANGLEWIDGET_SOURCES
+    ${CMAKE_CURRENT_LIST_DIR}/dialogdemangle.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogdemangle.ui
+    ${CMAKE_CURRENT_LIST_DIR}/xdemanglewidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xdemanglewidget.ui
+)
