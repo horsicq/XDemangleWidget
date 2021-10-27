764911bfba9ad1b78e3355c9ef7b90264a898c0d
horsicq <horsicq@gmail.com>
2021-10-27 18:33:12 +0200

    Fix: 2021-10-27

--- xdemanglewidget.cmake
+++ xdemanglewidget.cmake
@@ -1,8 +1,11 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-# TODO
+include(${CMAKE_CURRENT_LIST_DIR}/../XShortcuts/xshortcuts.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../XDemangle/xdemangle.cmake)
 
 set(XDEMANGLEWIDGET_SOURCES
+    ${XSHORTCUTS_SOURCES}
+    ${XDEMANGLE_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialogdemangle.cpp
     ${CMAKE_CURRENT_LIST_DIR}/dialogdemangle.ui
     ${CMAKE_CURRENT_LIST_DIR}/xdemanglewidget.cpp
